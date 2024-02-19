8b5d61b1b4a9977dd44746a3f1df78830b76db3f
Shelton Bilgates <sheltonbilgates@gmail.com>
2024-02-19 13:11:51 +0530

    Updates page.jsx of update-prompt

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -63,4 +63,10 @@ const UpdatePrompt = () => {
   );
 };
 
-export default UpdatePrompt;
+const EditPrompt = () => {
+  return <Suspense>
+      <UpdatePrompt />
+  </Suspense>
+}
+
+export default EditPrompt;
